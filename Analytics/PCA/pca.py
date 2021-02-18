c6f8d336bdc4b11c1f6d93ee3ae889d2cadccdf2
Zonitidus <zonitidus@gmail.com>
2021-02-17 21:02:29 -0500

    Solved PCA issues. Empty clusters in Kmeans

---
+++ Analytics/PCA/pca.py
@@ -0,0 +1,12 @@
+
+
+class pca():
+
+    def __init__(self, data, n, k):
+        
+        self.data = data
+        self.n = n
+        self.k = k
+    
+    def fit(self):
+        pass
--- Analytics/clustering/kmeans/KMeans.py
+++ Analytics/clustering/kmeans/KMeans.py
@@ -11,15 +11,18 @@ style.use('ggplot')
 
 data1 = pd.read_csv('Analytics/clustering/kmeans/datasets/buddymove_holidayiq.csv', header = None)
 #plt.scatter(data1[0].values, data1[1].values)
-print(data1.tail())
+#print(data1.tail())
 normalized_df = ((data1-data1.mean())/data1.std()).to_numpy()
+
+print('Uno\n')
+print(normalized_df)
 
 #Normalizacion de datos, para que se conserve el tipo de dato
 df_scaled = StandardScaler()
 df_scaled = pd.DataFrame(df_scaled.fit_transform(data1),columns  = data1.columns )
 print(df_scaled.tail())
 #prueba aparte (borrame)
-print('dato 1 \n',df_scaled[0][1])
+#print('dato 1 \n',df_scaled[0][1])
 class KMeans():
   
   """
@@ -46,24 +49,23 @@ class KMeans():
   """
 
   def pca_process(self, data):
-    x = data.iloc[:,0:6].values
-    y = data.iloc[:,5].values
-    print(x)
+
+
     #Calculo de la matrix de convarinza
     cov_mat = np.cov(data.T)
-    print('NumPy covariance matrix: \n%s' %cov_mat)
+    #print('NumPy covariance matrix: \n%s' %cov_mat)
 
     #Calculo de los eigenvector y eigenvalues
     eigen_vals, eigen_vecs = np.linalg.eig(cov_mat)
 
-    print('Eigenvectors \n%s' %eigen_vecs)
-    print('Eigenvalues \n%s' %eigen_vals)
+    #print('Eigenvectors \n%s' %eigen_vecs)
+    #print('Eigenvalues \n%s' %eigen_vals)
 
     #Listar y orden las parejas de Eigenvectors y Eigenvalues
     eigen_pairs =  [ (np.abs(eigen_vals[i]),eigen_vecs[:,i]) for i in range(len(eigen_vals))]
 
     eigen_pairs.sort(key = lambda x: x[0], reverse = True)
-    print('Eigenvalues en orden descendente')
+    #print('Eigenvalues en orden descendente')
     for i in eigen_pairs:
           print(i[0])
 
@@ -73,24 +75,33 @@ class KMeans():
 
     #continuamos generando la matrix de proyeccion a partir de los eigenvalues escogidos
     matrix_proyeccion = np.hstack((eigen_pairs[0][1].reshape(6,1),eigen_pairs[1][1].reshape(6,1)))
-    print('Matriz de Proyeccion:\n',matrix_proyeccion)
+    #print('Matriz de Proyeccion:\n',matrix_proyeccion)
     
     #por ultimo sacamos los nuevos componentes de los datos
     Y = data.dot(matrix_proyeccion)
     #mostramos
-    print(Y)
+    print('PCA resutltado \n', Y.to_numpy())
 
+    Y = Y.to_numpy()
     return Y
+    
   def fit(self, data):
     
     self.centroids = {}
     self.data = data
 
+
+    print('\n\n\n')
     #Se definen como centroides los primeros k elementos del dataset
-    i = 0
+    for centroid in range(self.k):
+      print('centroide: ', data[centroid])
+      self.centroids[centroid] = data[centroid] 
+    print('\n\n\n')
+    
+    """
     for centroid in range(self.k):
       self.centroids[centroid] = random.choice(data)
-
+    """
   def step(self, data):
 
     self.clasified_data = {}
@@ -108,12 +119,18 @@ class KMeans():
     #Se guarda una copia de los centroides anteriores
     prev_centroids = dict(self.centroids)
 
+    print(self.centroids)
+
+
+
     #Se redefinen los clusters con el promedio de los puntos que pertenecen a cada agrupación
     for key in self.clasified_data:
         
       #print(type(key), key)
+      print('count: ', self.clasified_data[key])
       self.centroids[key] = np.average(self.clasified_data[key], axis = 0)
-        
+    print(self.centroids)
+
     #Partimos del supuesto que los clusters son óptimos
     optimized = True
 
@@ -168,7 +185,7 @@ class KMeans():
 
 
 
-clf = KMeans(k = 6)
+clf = KMeans(k = 7)
 df_compress = clf.pca_process(df_scaled)
 clf.fit(df_compress)
 clf.step(clf.data)
@@ -180,9 +197,13 @@ for classification in clf.clasified_data:
     color = colors[classification]
     for featureset in clf.clasified_data[classification]:
         plt.scatter(featureset[0], featureset[1], color=color, s=80, linewidths=2)
+
+print(clf.centroids)
 for centroid in clf.centroids:
-    plt.scatter(clf.centroids[centroid][0], clf.centroids[centroid][1],
-                marker="x", color="k", s=150, linewidths=5)
+
+  plt.scatter(clf.centroids[centroid][0], clf.centroids[centroid][1],
+    marker="x", color="k", s=150, linewidths=5)
+                
 plt.show()
 
 
