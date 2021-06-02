b5c282a4dc4e330112919224689400450436a877
Zonitidus <zonitidus@gmail.com>
2021-06-02 12:46:29 -0500

    Added Kprototypes upload and data format methods

--- Analytics/clustering/model/controller.py
+++ Analytics/clustering/model/controller.py
@@ -10,6 +10,7 @@ from Analytics.PCA import pca
 from Analytics.clustering.kmeans import KMeans
 from Analytics.Visualization import graphs
 from Analytics.clustering.Pruebas.datasets.routes import folder
+from Analytics.clustering.kprototypes.KPrototypes import KPrototypes
 
 
 class NumpyEncoder(json.JSONEncoder):
@@ -32,6 +33,7 @@ def fit_data(filename, k=3):
     df_scaled = StandardScaler()
     df_scaled = pd.DataFrame(df_scaled.fit_transform(data1), columns=data1.columns)
 
+
     km = KMeans(k=k)
     comp = pca(2)
     dat = comp.fit(df_scaled)
@@ -48,4 +50,46 @@ def fit_data(filename, k=3):
 
     return dumped
 
+def fit_data_kp(filename, k=3):
+    route = folder + filename
+    data1 = pd.read_csv(route, sep=',')
 
+    print(data1)
+
+    normalized_df = ((data1 - data1.mean()) / data1.std()).to_numpy()
+
+    clf = KPrototypes(cat=[1], k=3)
+    clf.fit(normalized_df)
+
+    for i in range(50):
+        clf.step(clf.data)
+
+    df_kp = pd.DataFrame()
+
+    for classification in clf.clasified_data:
+        df_temp = pd.DataFrame(clf.clasified_data[classification])
+        df_kp = df_kp.append(df_temp)
+
+    print(clf.clasified_data)
+
+    comp = pca(2)
+
+    dat = comp.fit(df_kp)
+
+    count = 0
+    print(clf.clasified_data)
+
+    blist = []
+
+    for classification in clf.clasified_data:
+        list = []
+        for featureset in clf.clasified_data[classification]:
+            temp_dic = {}
+            temp_dic['x'] = dat[count][0]
+            temp_dic['y'] = dat[count][1]
+            list.append(temp_dic)
+            count += 1
+
+        blist.append(list)
+
+    return blist
--- run.py
+++ run.py
@@ -5,7 +5,7 @@ from Analytics.clustering.Pruebas.datasets.routes import folder
 from Analytics.clustering.model import controller
 
 import os
-from flask import Flask, flash, request, redirect, url_for, render_template
+from flask import Flask, flash, request, redirect, url_for, render_template, jsonify
 from werkzeug.utils import secure_filename
 
 
@@ -58,4 +58,28 @@ def upload_file():
             json = controller.fit_data('/'+filename)
 
             return json
+    return 'Oh no'
+
+
+@app.route('/uplkp', methods=['GET', 'POST'])
+def upload_file_kp():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+            data = controller.fit_data_kp('/'+filename)
+
+            return jsonify(data)
+
     return 'Oh no'
